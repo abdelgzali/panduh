22691dde75770912591368764cf3b313146e7426
Abdalkadir <abdelgzali@gmail.com>
2020-11-11 23:14:04 -0800

    recipe modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const modal = () => {
-  return ( 
-    <div></div>
-   );
-}
- 
-export default modal;
+const Modal = ({showModal, toggleModal, content}) => {
+  const modalClassName = showModal ? "modal display-block" : "modal display-none";
+  return (
+    <div className={modalClassName}>
+      <section className="modal-body">
+        <h3>{content.title}</h3>
+        <button onClick={() => {toggleModal(!showModal)}}>x</button>
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+import Modal from './Modal';
 
 function RecipeCard({recipe}) {
+  const [modalToggled, toggleModal] = useState(false);
   return (
     <div className="recipe-card">
       {recipe.title}
       <img src={recipe.image} alt="recipe-image"></img>
+      <button onClick={() => {toggleModal(!modalToggled)}}>show more</button>
+      <Modal showModal={modalToggled} toggleModal={toggleModal} content={recipe}></Modal>
     </div>
   )
 }
